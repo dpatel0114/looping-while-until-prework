33a0c4d5d592287eca6c2a9afcef21947fbe4f8d
Dhara Patel <1991dharapatel@gmail.com>
2019-03-16 22:03:54 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,8 @@
 def using_while
-	levitation_force = 6
-	
-	#your code here
+	while levitation_force<10 
+	puts "the desired phrase while your levitation force less than 10"
+	levitation_force +=1
+end	
 end
 
 
